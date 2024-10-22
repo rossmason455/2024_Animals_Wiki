a09a181eda791e23f21450b0fbb5b37ab2a2bee5
Ross Mason (Student) <N00230645@iadt.ie>
2024-10-22 15:24:58 +0100

    edited navigation.blade.php: added route to animal.index, edited web.php: imported AnimalController, edited AnimalController: added to the index method, edited index.blade.php: added database vars to card and added card components

--- database/migrations/2024_10_21_122459_create_animals_table.php
+++ database/migrations/2024_10_21_122459_create_animals_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('animals', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->integer('year');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
