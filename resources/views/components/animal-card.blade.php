cac13bd29122aa4d97a10b054d2aa34c1a3ed8dd
Ross Mason <N00230645@iadt.ie>
2024-11-11 23:39:38 +0000

    added tiger to db and removed all fields except name from animal-card.blade

--- resources/views/components/animal-card.blade.php
+++ resources/views/components/animal-card.blade.php
@@ -15,13 +15,6 @@
     <img src="{{ asset($image) }}" alt="{{ $animal_name }}" class="w-full h-60 object-cover">
     <div class="p-4">
         <h3 class="font-bold text-lg">{{ $animal_name }}</h3>
-        <p class="text-gray-600"><strong>Scientific Name:</strong> {{ $scientific_name }}</p>
-        <p class="mt-2 text-gray-700">{{ $description }}</p>
-        <p class="mt-2 text-gray-600"><strong>Behavioral Notes:</strong> {{ $behavioral_notes }}</p>
-        <p class="mt-2 text-gray-600"><strong>Lifespan:</strong> {{ $lifespan }}</p>
-        <p class="mt-2 text-gray-600"><strong>Diet:</strong> {{ $diet }}</p>
-        <p class="mt-2 text-gray-600"><strong>Social Structure:</strong> {{ $social_structure }}</p>
-        <p class="mt-2 text-gray-600"><strong>Threats:</strong> {{ $threats }}</p>
-        <p class="mt-2 text-gray-600"><strong>Primary Predator:</strong> {{ $primary_predator }}</p>
+
     </div>
 </div>
