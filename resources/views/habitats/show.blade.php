c36135f32b30e57e06a4a8ccf6d952ab3c279d7b
Ross Mason <N00230645@iadt.ie>
2024-12-10 15:44:41 +0000

    modified habitats show.blade file

---
+++ resources/views/habitats/show.blade.php
@@ -0,0 +1,39 @@
+<div>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ $habitat->habitat_name }} Details
+            </h2>
+        </x-slot>
+
+        <div class="py-12"
+            style="background-image: url('{{ asset('images/habitat-background.jpg') }}'); background-size: contain; h60 ">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <x-habitat-detail :habitat_name="$habitat->habitat_name"
+                                    :description="$habitat->description" :climate="$habitat->climate"
+                                  :image="$habitat->image_url" :created_at="$habitat->created_at"
+                            :updated_at="$habitat->updated_at" />
+
+
+                        <div class="mt-4">
+                            <a href="{{ route('habitats.edit', $habitat->id) }}" class="text-blue-500 hover:underline">
+                                Edit habitat
+                            </a>
+
+
+                        </div>
+
+                        <form action="{{ route('habitats.destroy', $habitat->id) }}" method="POST"
+                            onsubmit="return confirm('Are you sure you want to delete this habitat?');">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="text-red-600 hover:underline">Delete habitat</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+</div>
