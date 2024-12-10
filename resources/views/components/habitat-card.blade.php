1582cd0affaa1799e8121e8644a6df40937edf98
Ross Mason <N00230645@iadt.ie>
2024-12-10 15:10:28 +0000

    modified habitat-card.blade

--- resources/views/components/habitat-card.blade.php
+++ resources/views/components/habitat-card.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+@props([
+'habitat_name',
+'description',
+'climate',
+'image'
+])
+
+<div class="bg-white rounded-lg shadow-md overflow-hidden">
+    <img src="{{ asset($image) }}" alt="{{ $habitat_name }}" class="w-full object-cover">
+    <div class="p-4">
+        <h3 class="font-bold text-lg">{{ $habitat_name }}</h3>
+
+    </div>
 </div>
