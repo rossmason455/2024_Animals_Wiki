e828a6a5643de74d62d3d21b0b129dd3115d690c
Ross Mason <N00230645@iadt.ie>
2024-12-10 15:25:02 +0000

    modified habitat-form.blade

--- resources/views/components/habitat-form.blade.php
+++ resources/views/components/habitat-form.blade.php
@@ -1,3 +1,61 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+@props(['action', 'method'])
+
+<form action="{{ $action }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    @if($method === 'PUT' || $method === 'PATCH')
+    @method($method)
+    @endif
+
+    <!-- habitat Name -->
+    <div class="mb-4">
+        <label for="habitat_name" class="block text-sm text-gray-700">habitat Name</label>
+        <input type="text" name="habitat_name" id="habitat_name"
+            value="{{ old('habitat_name', $habitat->habitat_name ?? '') }}" required
+            class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" />
+        @error('habitat_name')
+        <p class="text-sm text-red-600">{{ $message }}</p>
+        @enderror
+    </div>
+
+
+
+    <!-- description -->
+    <div class="mb-4">
+        <label for="description" class="block text-sm text-gray-700">description</label>
+        <textarea name="description" id="description"
+            class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">{{ old('description', $habitat->description ?? '') }}</textarea>
+        @error('description')
+        <p class="text-sm text-red-600">{{ $message }}</p>
+        @enderror
+    </div>
+
+    <!-- Climate -->
+    <div class="mb-4">
+        <label for="climate" class="block text-sm text-gray-700">Climate</label>
+        <textarea name="climate" id="climate"
+            class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">{{ old('climate', $habitat->climate ?? '') }}</textarea>
+        @error('climate')
+        <p class="text-sm text-red-600">{{ $message }}</p>
+        @enderror
+    </div>
+
+
+
+
+
+
+
+
+    @isset($habitat->image_url)
+    <div class="mb-4">
+        <img src="{{ asset($habitat->image_url) }}" alt="habitat image" class="w-24 h-32 object-cover" />
+    </div>
+    @endisset
+
+    <!-- Submit Button -->
+    <div>
+        <x-primary-button>
+            {{ isset($habitat) ? 'Update habitat' : 'Add habitat' }}
+        </x-primary-button>
+    </div>
+</form>
